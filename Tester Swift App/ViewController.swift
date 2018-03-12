953a6987eed0fc9613359895bb25a6b94d8bb547
Shamar Smith-Standford <shamarss@icloud.com>
2018-03-12 18:39:03 -0400

    Boolean tings

--- Tester Swift App/ViewController.swift
+++ Tester Swift App/ViewController.swift
@@ -9,30 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-   
+    
     @IBOutlet weak var theLabel: UILabel!
     
     @IBOutlet weak var text1: UITextField!
-   
+    
     @IBOutlet weak var text2: UITextField!
     
     var pressCount = 0
     
     @IBAction func buttonPressed(_ sender: AnyObject) {
         
-        theLabel.text = "Answer: \(Double(text1.text!)! + Double (text2.text!)!)"
+        let addition = true
         
+        if addition {
+            
+            theLabel.text = "Answer: \(Double(text1.text!)! + Double (text2.text!)!)"
+        } else {
+            theLabel.text = "Answer: \(Double(text1.text!)! - Double (text2.text!)!)"
+        }
     }
     
     
     
     override func viewDidLoad() {
-    super.viewDidLoad()
+        super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-       }
-
+    }
+    
     override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
+        super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 }
